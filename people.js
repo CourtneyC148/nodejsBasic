40d9520fe1d05d61f94c1cd75b0799538a98b2cd
courtneyc148 <courtneyc148@gmail.com>
2020-11-16 23:12:29 -0500

    update

---
+++ people.js
@@ -0,0 +1,12 @@
+
+const people = ['David', 'Sara', 'mario', 'Carmen'];
+const ages = [20, 25, 30, 35];
+
+// console.log(people);
+
+// module.exports = people;
+
+module.exports = {
+  people,
+  ages,
+}
